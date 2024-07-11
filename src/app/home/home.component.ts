7de677ff91e3f0772a23b251137136dcd55a0b78
Thereaper200 <113812163+Thereaper200@users.noreply.github.com>
2024-07-11 02:37:21 -0700

    Proyecto Angular

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,42 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service/api.service';
 import { FormGroup, FormControl } from '@angular/forms';
+
 @Component({
-selector: 'app-home',
-templateUrl: './home.component.html',
-styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-myForm: FormGroup = new FormGroup({});
-name: string =
-"";
-showImage: boolean = false;
-data: any = {};
-constructor(private apiService:ApiService){
-this.myForm = new FormGroup({
-name: new FormControl(''),
-});
+    myForm: FormGroup = new FormGroup({});
+    name: string = "";
+    showImage: boolean = false;
+    data: any = {};
+    constructor(private apiService:ApiService){
+    this.myForm = new FormGroup({
+    name: new FormControl(''),
+    });
 }
+
 ngOnInit(): void {
+
 }
+
 onSubmit(){
-const formValue = this.myForm.value;
-this.name= formValue.name;
-this.consultPokemon(this.name);
-this.myForm.reset();
+    const formValue = this.myForm.value;
+    this.name= formValue.name;
+    this.consultPokemon(this.name);
+    this.myForm.reset();
 }
+
 consultPokemon(pokemon:string){
-this.apiService.getData(pokemon).subscribe({
-next: this.handleSuccessMethod.bind(this),
-error:this.handleErrorMethod.bind(this),
-})
+    this.apiService.getData(pokemon).subscribe({
+        next: this.handleSuccessMethod.bind(this),
+        error:this.handleErrorMethod.bind(this),
+    })
 }
+
 handleSuccessMethod(data:any){
-this.showImage = true;
-this.data = data;
+    this.showImage = true;
+    this.data = data;
 }
+
 handleErrorMethod(){
-this.showImage = false;
-this.data.name = "Pokemon no encontrado"
-}
+        this.showImage = false;
+        this.data.name = "Pokemon no encontrado"
+    }
 }
